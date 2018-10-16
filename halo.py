3cb31604f54be6636a094b6893396127a146c902
Endy Muhardin <endy.muhardin@gmail.com>
2018-10-16 07:09:55 +0700

    hello world

---
+++ halo.py
@@ -0,0 +1,6 @@
+# menampilkan tulisan ke layar
+print ("belajar python")
+print("statement adalah perintah yang harus dijalankan oleh komputer")
+print("comment adalah penjelasan supaya kita mengerti")
+print("comment diawali dengan tanda #")
+print("semua yang diawali tanda # akan diabaikan/tidak dieksekusi oleh komputer")
